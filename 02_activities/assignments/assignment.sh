02308626a29e7833a08f231b4fd02f760bed5e8b
Rahul Nanda <rahulnanda27@gmail.com>
2025-07-30 22:28:42 -0400

    sixth attempt 1 guess

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,7 +43,9 @@ cd processed
 mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ./data/raw/*server*.log ./data/processed/server_logs
+unzip rawdata.zip /c/Users/Minal/DSI/day2/shell/02_activities/assignments/data/raw
+cd rawdata
+cp ./*server*.log /c/Users/Minal/DSI/day2/shell/02_activities/assignments/data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
 cp ./data/raw/*server*.log ./data/processed/user_logs
