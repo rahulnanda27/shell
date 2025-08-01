58b7fbf8064876ff5bc12147ede6dbae1f488266
Rahul Nanda <rahulnanda27@gmail.com>
2025-07-31 21:24:52 -0400

    omg

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -45,7 +45,7 @@ mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ..
 cd raw
-cp ./*server*.log ../data/processed/server_logs/
+cp ./*server*.log ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
 cp ./data/raw/*server*.log ./data/processed/user_logs
